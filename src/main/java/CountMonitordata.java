bb00ed7f468d68faf463c6015630b3d67a06f3da
Sunny <823319298@qq.com>
2020-07-11 01:41:36 +0800

    分组统计

--- src/main/java/CountMonitordata.java
+++ src/main/java/CountMonitordata.java
@@ -2,6 +2,10 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.filter.CompareFilter;
+import org.apache.hadoop.hbase.filter.Filter;
+import org.apache.hadoop.hbase.filter.RegexStringComparator;
+import org.apache.hadoop.hbase.filter.RowFilter;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
 import org.apache.hadoop.hbase.protobuf.ProtobufUtil;
@@ -25,27 +29,64 @@ import scala.Tuple2;
 import java.io.IOException;
 import java.util.*;
 
-public class CountMonitordata {
+public abstract class CountMonitordata {
 
+    private static Dataset<Row> ResultDF;
     private static transient SparkSession sc;
     private static transient Configuration inConf;
     private static transient Scan scan;
+
+    public static Dataset<Row> getResultDF() {
+        return ResultDF;
+    }
+
+    public static void setResultDF(Dataset<Row> resultDF) {
+        ResultDF = resultDF;
+    }
+
+    public static SparkSession getSc() {
+        return sc;
+    }
+
+    public static void setSc(SparkSession sc) {
+        CountMonitordata.sc = sc;
+    }
+
+    public static Configuration getInConf() {
+        return inConf;
+    }
+
+    public static void setInConf(Configuration inConf) {
+        CountMonitordata.inConf = inConf;
+    }
+
+    public static Scan getScan() {
+        return scan;
+    }
+
+    public static void setScan(Scan scan) {
+        CountMonitordata.scan = scan;
+    }
 
     /**
      * 初始化Spark执行环境
      * @throws IOException
      */
-    private static void initSparkContext() throws IOException {
+    private static void initSparkContext(String data) throws IOException {
     	SparkConf sparkConf = new SparkConf().setAppName("SparkSql countMon").setMaster("local[*]");
 //        SparkConf sparkConf = new SparkConf().setAppName("SparkSql countMon").set("spark.port.maxRetries","100");
         //修改序列化方式
         sparkConf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
+        sparkConf.set("spark.debug.maxToStringFields","52428800");
         //SparkSession替代JavaSparkContext
         sc = SparkSession.builder().config(sparkConf).getOrCreate();
 
         inConf = HBaseConfiguration.create();
         scan = new Scan();
 
+        //rowkey匹配正则表达式
+        Filter filter = new RowFilter(CompareFilter.CompareOp.EQUAL,new RegexStringComparator(".{1}"+data));
+        scan.setFilter(filter);
         //将scan编码
         ClientProtos.Scan proto = ProtobufUtil.toScan(scan);
         String scanToString = Base64.encodeBytes(proto.toByteArray());
@@ -68,7 +109,9 @@ public class CountMonitordata {
         //newAPIHadoopRDD这个RDD用于读取存储在Hadoop中的数据，文件有新 API 输入格式和额外的配置选项，传递到输入格式
         //参数conf会被广播，不可以修改，所以最好一个RDD一个conf
         JavaPairRDD<ImmutableBytesWritable, Result> hbaseRDD = jsc.newAPIHadoopRDD(inConf,TableInputFormat.class, ImmutableBytesWritable.class, Result.class);
+
         //Scan.set这里可以加一些scan条件，读hbase
+
         //使用map函数将JavaPairRDD=>JavaRDD,反之使用mapToPair函数
         JavaRDD<Row> dataRDD = hbaseRDD.map(new Function<Tuple2<ImmutableBytesWritable,Result>,Row>() {
             //序列化表示UID，用于类的版本控制
@@ -109,45 +152,47 @@ public class CountMonitordata {
         //展示schema
         HBaseDF.printSchema();
 
-        //HBaseDF.createOrReplaceTempView("monitor");
         StringBuilder sql = new StringBuilder();
         sum=1;
         for(int i=0;i<24;++i){
             for(int j=0;j<60&&sum<1441;++j){
-                sql.append(", 'R").append(String.format("%02d",i)).append(String.format("%02d",j)).append("', R").append(String.format("%02d",i)).append(String.format("%02d",j));
+                sql.append(", '").append(String.format("%02d",i)).append(String.format("%02d",j)).append("', R").append(String.format("%02d",i)).append(String.format("%02d",j));
                 ++sum;
             }
         }
-        Dataset<Row>  ResultDF = HBaseDF.selectExpr("rowkey","stack(1440"+sql+") as (`times`,`values`)").orderBy("rowkey","times");
-        ResultDF.show();
+        //DataFrame 行列转换
+        ResultDF = HBaseDF.selectExpr("rowkey","stack(1440"+sql+") as (`times`,`values`)").orderBy("rowkey","times");
 
-//        StringBuilder sql = new StringBuilder("select rowKey");
-//        sum=1;
-//        for(int i=0;i<24;++i){
-//            for(int j=0;j<60&&sum<1441;++j){
-//                sql.append(",R").append(String.format("%02d", i)).append(String.format("%02d", j));
-//                ++sum;
-//            }
-//        }
-//        Dataset<Row> newDF = sc.sql(sql + " from monitor");
-//        newDF.show();
+        //给ResultDF视图命名，方便后续sql操作
+        ResultDF.createOrReplaceTempView("monitor");
+    }
+
+    /**
+     * 写SQL
+     */
+    private static void process(){
+
+        //String sql = "Select SUBSTRING(rowkey,8,5),Count(values) As Count FROM monitor where values is not null group by SUBSTRING(rowkey,8,5)";
+        String sql = "Select substring(rowkey,8,5) as monitor,count(times) as num FROM monitor where values is not null group by substring(rowkey,8,5)";
+
+        Dataset<Row> newDF = sc.sql(sql);
+        newDF.show();
 //        String mysqlUrl = "jdbc:mysql://192.168.****";
 //        String writeTable = "HBase_Spark_teacher";
 //        Properties connectionProperties = new Properties();
 //        connectionProperties.setProperty("user", "root");
 //        connectionProperties.setProperty("password", "root");
-//        newDF.write().jdbc(mysqlUrl,writeTable,connectionProperties);
-
+//        ResultDF.write().jdbc(mysqlUrl,writeTable,connectionProperties);
     }
 
     public static void main(String[] args) throws IOException {
         try {
-            initSparkContext();
+            initSparkContext("200624");
             prepareData(JavaSparkContext.fromSparkContext(sc.sparkContext()));
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-//        process();
+        process();
     }
 }
