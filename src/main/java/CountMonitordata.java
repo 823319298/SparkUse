346d53cada1fad5e4730ac4d372ea02797e40b2d
Sunny <823319298@qq.com>
2020-07-10 22:18:41 +0800

    转置

--- src/main/java/CountMonitordata.java
+++ src/main/java/CountMonitordata.java
@@ -38,8 +38,10 @@ public class CountMonitordata {
     private static void initSparkContext() throws IOException {
     	SparkConf sparkConf = new SparkConf().setAppName("SparkSql countMon").setMaster("local[*]");
 //        SparkConf sparkConf = new SparkConf().setAppName("SparkSql countMon").set("spark.port.maxRetries","100");
-        sparkConf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");//修改序列化方式
-        sc = SparkSession.builder().config(sparkConf).getOrCreate();//SparkSession替代JavaSparkContext
+        //修改序列化方式
+        sparkConf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
+        //SparkSession替代JavaSparkContext
+        sc = SparkSession.builder().config(sparkConf).getOrCreate();
 
         inConf = HBaseConfiguration.create();
         scan = new Scan();
@@ -50,8 +52,9 @@ public class CountMonitordata {
 
         inConf.setLong("hbase.client.keyvalue.maxsize", 52428800);   //主要应对超长列
         inConf.set("hbase.client.scanner.timeout.period", "1800000");
-        //把要查询的表视图化
+        //指定输入表名
         inConf.set(TableInputFormat.INPUT_TABLE, "his:DataCompression");
+        //Base-64编码的Scanner
         inConf.set(TableInputFormat.SCAN,scanToString);
 
         System.out.println("initSparkContext() success!!!!");
@@ -62,10 +65,15 @@ public class CountMonitordata {
      * 准备数据
      */
     private static void prepareData(JavaSparkContext jsc) throws IOException {
-        //hbase数据转RDD
+        //newAPIHadoopRDD这个RDD用于读取存储在Hadoop中的数据，文件有新 API 输入格式和额外的配置选项，传递到输入格式
+        //参数conf会被广播，不可以修改，所以最好一个RDD一个conf
         JavaPairRDD<ImmutableBytesWritable, Result> hbaseRDD = jsc.newAPIHadoopRDD(inConf,TableInputFormat.class, ImmutableBytesWritable.class, Result.class);
+        //Scan.set这里可以加一些scan条件，读hbase
+        //使用map函数将JavaPairRDD=>JavaRDD,反之使用mapToPair函数
         JavaRDD<Row> dataRDD = hbaseRDD.map(new Function<Tuple2<ImmutableBytesWritable,Result>,Row>() {
+            //序列化表示UID，用于类的版本控制
             private static final long serialVersionUID = 1L;
+            //重写call（）函数，返回Row类型，这部分需要根据自己需求将result中的数据封装为Object[]
             @Override
             public Row call(Tuple2<ImmutableBytesWritable, Result> tuple2) {
                 Result result = tuple2._2;
@@ -78,11 +86,13 @@ public class CountMonitordata {
                         ++sum;
                     }
                 }
+                //creat()方法参数（object... value）
                 return RowFactory.create((Object[]) values);
             }
         });
 
         List<StructField> structFields= new ArrayList<>();
+        //创建StructField,基本等同于list内有几个StructField就是几列，需要和上面的Row的Object[]对应
         structFields.add(DataTypes.createStructField("rowKey", DataTypes.StringType, true));
         int sum=1;
         for(int i=0;i<24;++i){
@@ -92,24 +102,35 @@ public class CountMonitordata {
             }
         }
 
-        //构建schema
+        //构建schema，可以把它理解为架子，结构
         StructType schema = DataTypes.createStructType(structFields);
-        //生成DataFrame
+        //生成DataFrame，把书放入架子，把数据放入结构里就变成了dataframe
         Dataset<Row> HBaseDF = sc.createDataFrame(dataRDD, schema);
-        //df相关操作
+        //展示schema
         HBaseDF.printSchema();
 
-        HBaseDF.createOrReplaceTempView("monitor");
-        StringBuilder sql = new StringBuilder("select rowKey");
+        //HBaseDF.createOrReplaceTempView("monitor");
+        StringBuilder sql = new StringBuilder();
         sum=1;
         for(int i=0;i<24;++i){
             for(int j=0;j<60&&sum<1441;++j){
-                sql.append(",R").append(String.format("%02d", i)).append(String.format("%02d", j));
+                sql.append(", 'R").append(String.format("%02d",i)).append(String.format("%02d",j)).append("', R").append(String.format("%02d",i)).append(String.format("%02d",j));
                 ++sum;
             }
         }
-        Dataset<Row> newDF = sc.sql(sql + " from monitor");
-        newDF.show();
+        Dataset<Row>  ResultDF = HBaseDF.selectExpr("rowkey","stack(1440"+sql+") as (`times`,`values`)").orderBy("rowkey","times");
+        ResultDF.show();
+
+//        StringBuilder sql = new StringBuilder("select rowKey");
+//        sum=1;
+//        for(int i=0;i<24;++i){
+//            for(int j=0;j<60&&sum<1441;++j){
+//                sql.append(",R").append(String.format("%02d", i)).append(String.format("%02d", j));
+//                ++sum;
+//            }
+//        }
+//        Dataset<Row> newDF = sc.sql(sql + " from monitor");
+//        newDF.show();
 //        String mysqlUrl = "jdbc:mysql://192.168.****";
 //        String writeTable = "HBase_Spark_teacher";
 //        Properties connectionProperties = new Properties();
